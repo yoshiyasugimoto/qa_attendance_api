eab7000c62e43b8531b9b26879dd3e0b4c15e3a5
Yusuke <yusuke.hamaike@mlabs.jp>
2019-05-15 20:38:24 +0900

    一旦完成

--- counter_up.py
+++ counter_up.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine, Column, String, Integer, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+pymysql://root:@localhost/question?charset=utf8')
+engine = create_engine(
+    'mysql+pymysql://root:task-wktk@/question?unix_socket=/cloudsql/mlab-apps:asia-northeast1:mlab-apps-sql')
 
 Base = declarative_base()
 Base.metadata.create_all(engine)
@@ -23,5 +24,6 @@ def add_question():
             i.count = 5
             session.commit()
             session.close()
+
 
 add_question()
